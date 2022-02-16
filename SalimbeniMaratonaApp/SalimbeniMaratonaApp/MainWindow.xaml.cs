9f3ac25ea50547b1af796670082ed8b313f88f69
AlbertoSalimbeni <97670975+AlbertoSalimbeni@users.noreply.github.com>
2022-02-16 14:14:18 +0100

    carico file

---
+++ SalimbeniMaratonaApp/SalimbeniMaratonaApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SalimbeniMaratonaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ElencoMaratone elenco;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            elenco = new ElencoMaratone();
+            DgElencoMaratone.ItemsSource = elenco.Elenco;
+        }
+
+        private void BtnLeggiFile_Click(object sender, RoutedEventArgs e)
+        {
+            elenco.LeggiDaFile();
+            DgElencoMaratone.Items.Refresh();
+        }
+
+
+
+
+
+    }
+}
---
+++ SalimbeniMaratonaApp/SalimbeniMaratonaApp/Maratona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalimbeniMaratonaApp
+{
+    class Maratona
+    {
+        public string Nome { get; set; }
+        public string Società { get; set; }
+        public int TempoInMinuti { get; set; }
+        public string Città { get; set; }
+
+    }
+}
